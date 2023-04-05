a0aac8c7de948a7d1a6402fb43f06a614410c016
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-05 21:40:40 +0000

    tested it before

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,30 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Author: moamineayadi
+*/
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - len
+ *
+ * @s: char
+ *
+ * Return 0
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	int ch;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		ch = _strlen_recursion(s + 1);
+	}
+	return (ch + 1);
+}
