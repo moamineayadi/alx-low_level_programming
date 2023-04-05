c118e390488bac068d8eb863b33ececb8560da9d
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-05 22:36:19 +0000

    i hate this

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-pow_recursion.c
+ * Author: moamineayadi
+ */
+
+#include "main.h"
+
+
+/**
+ * _pow_recursion - return x the power of y
+ *
+ * @x: int
+ * @y: int
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else {
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
