e6e46fb7fabc3dee4dc0bc7250c91245f755c544
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-05 21:48:16 +0000

    retrying

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,28 @@
+/*
+ * file: 3-factorial.c
+ * Author: moamineayadi
+ */
+
+#include "main.h"
+
+
+/**
+ * factorial - return factorial of a number
+ *
+ * @n: int to return his factorial
+ *
+ * Return: int
+ *
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
