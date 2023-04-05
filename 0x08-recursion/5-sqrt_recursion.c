d8d8e050545275b97133ca27acf879d1bc3bbd54
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-05 22:43:12 +0000

    added task sqrt rec

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+/*
+ * file: 5-sqer_recursion.c
+ * Author: moamineayadi
+ */
+
+#include "main.h"
+
+/*
+ * _sqrt_recursion - Return sqrt of number
+ *
+ * @n: int
+ *
+ * Return: int
+ */
+
+
+int _sqrt_recursion(int n)
+{
+	return (0.5 ** n);
+}
