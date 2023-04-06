7b436dccf2cb99c64dde8e829ea6f5dcececc775
Linux User <maminou@ATLAS-V3NFP7ROS.localdomain>
2023-04-06 02:38:48 +0000

    i hope it works

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-puts_recursion.c
+ * Author: moamineayadi
+*/
+
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string
+ * @c: char
+ *
+ * Return: Nothing
+*/
+
+void _puts_recursion(char *c)
+{
+    if (*c == '\0')
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        _putchar(*c);
+        c++;
+        _puts_recursion(c);
+    }
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+/*
+ * File: main.h
+ * Author: moamineayadi
+*/
+
+int _putchar(char c);
+void _puts_recursion(char *s);
