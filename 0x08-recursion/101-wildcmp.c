1c9c51623b7efb0358fc004de7c0d0c8bf7c525a
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-06 02:45:54 +0000

    added final task

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,13 +15,21 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == *s1)
+	if (*s1 == '\0')
 	{
-		return (wildcmp(s1 + 1, s2 +1));
+		if (*s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return(*s2 == '\0');
 	}
-	if (*s1 == '\0' || *s2 == '\0')
+	if (*s2 == "*")
 	{
-		return (1);
+		return (wildcmp(s1 + 1, s2));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
