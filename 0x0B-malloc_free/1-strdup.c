91cd07658e803df01fdbb727e3e17cee8985de43
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-12 12:51:32 -0700

    inshallah te5dem

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+/*
+ * File : 1-strdup.c
+ * Author : moamineayadi
+ */
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *
+ * _strdup - return a pointer to mem
+ * @str: string
+ *
+ * Return: char
+ *
+ */
+
+char *_strdup(char *str)
+{
+	char *pointer;
+	int size = 0;
+
+	if (str == NULL)
+	{
+		retrun (NULL)
+	}
+
+	while (str[size])
+	{
+		size++;
+	}
+
+	pointer = malloc(sizeof(char) * size +1 );
+
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+
+	size = 0;
+	while (str[size])
+	{
+		pointer[size] = str[size];
+		size++;
+	}
+	return (pointer);
+}
