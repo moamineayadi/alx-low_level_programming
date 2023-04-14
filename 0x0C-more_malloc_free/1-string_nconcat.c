e6282dcfa1e07c1dbcca86f03a0197cd8fdb29cd
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-14 11:17:17 +0000

    fixes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,27 +18,39 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int len1, len2;
-char *concat;
+	unsigned int len1, len2;
+	char *concat;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
 
-len1 = strlen(s1);
-len2 = strlen(s2);
+	if (s2 == NULL)
+	{	
+		s2 = "";
+	}
 
-if (n >= len2)
-n = len2;
 
-concat = malloc(len1 + n + 1);
-if (concat == NULL)
-return (NULL);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
-memcpy(concat, s1, len1);
-memcpy(concat + len1, s2, n);
-concat[len1 + n] = '\0';
+	if (n >= len2)
+	{
+		n = len2;
+	}
 
-return (concat);
+
+	concat = malloc(len1 + n + 1);
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	
+
+	memcpy(concat, s1, len1);
+	memcpy(concat + len1, s2, n);
+	concat[len1 + n] = '\0';
+
+	return (concat);
 }
