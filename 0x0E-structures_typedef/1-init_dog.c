760bf358e68164cf31989f462570a1eb59b9146e
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-17 22:20:14 +0000

    why fails on check4

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,16 +11,15 @@
  * @name: pointer to the name of the dog
  * @age: age of the dog
  * @owner: pointer to the owner of the dog
- *
- * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d == NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		d = malloc(sizeof(struct dog));
 	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
