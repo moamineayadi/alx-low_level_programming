32d11d767184cb92c0e304d9628f052a032e6ba2
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-17 21:10:07 +0000

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,16 +3,24 @@
 * Author: moamineayadi
 */
 
-/**
- * struct dog - A struct representing a dog
- * @name: The dog's name
- * @age: The dog's age
- * @owner: The dog's owner's name
- */
+#ifndef _DOG_H
+#define _DOG_H
 
-struct dog
+/**
+ * struct dog - a new type
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}
+	dog;
+
+int _putchar(char c);
+
+#endif /* DOG */
