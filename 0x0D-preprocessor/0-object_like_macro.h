347c3f028c35ecd61a745fd5788449f9e060be14
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-17 23:40:58 +0000

    init

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+/*
+* File: 0-object_like_macro.h
+* Author: moamineayadi
+*/
+
+#ifndef _OBJECT_LIKE_MACRO_H_
+#define _OBJECT_LIKE_MACRO_H_
+
+#define SIZE 1024
+
+#endif /* _OBJECT_LIKE_MACRO_H_ */
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+# chatgpting my life
