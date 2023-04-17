521d78382a1e8a6a43b138566fc5d857c68bee38
moamineayadi <mohamedamineayadi@icloud.com>
2023-04-17 21:18:28 +0000

    added 1-init_dogs.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,28 @@
+/*
+* File: 1-init_dog.c
+* Author: moamineayadi
+*/
+
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ *
+ * @d: pointer
+ * @name: string
+ * @age: float
+ * @owner: string
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
